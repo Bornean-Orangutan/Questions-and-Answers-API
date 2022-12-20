f213e7380cc29c9736151c747adac81fa6c1298c
Jonathon Zimdars <zimdarsj@kenyon.edu>
2022-12-20 17:34:52 -0600

    Testing File Loading

--- server/database/etl/loadQuestions.js
+++ server/database/etl/loadQuestions.js
@@ -3,9 +3,57 @@ const fs = require('fs');
 const { parse } = require('csv-parse');
 const csv = require('csv-parser');
 
+const nReadlines = require('n-readlines');
+const questionLines = new nReadlines('./server/database/etl/questions.csv');
 
+let line;
+let lineNumber = 2;
 
+const allData = [];
 
+// const createData = (data) => {
+//   Question.create({
+//     id: Number(data[0]),
+//     product_id: Number(data[1]),
+//     body: data[2],
+//     date: Date(data[3]),
+//     asker_name: data[4],
+//     asker_email: data[5],
+//     reported: Boolean(Number(data[6])),
+//     helpfulness: Number(data[7])
+//   })
+// }
+
+line = questionLines.next();
+while (line = questionLines.next()) {
+  //console.log(`Line ${lineNumber} has: ${line.toString('ascii')}`);
+  //createData(line.toString('ascii').split(','));
+  const data = line.toString('ascii').split(',');
+  allData.push({
+    id: Number(data[0]),
+    product_id: Number(data[1]),
+    body: data[2],
+    date: Date(data[3]),
+    asker_name: data[4],
+    asker_email: data[5],
+    reported: Boolean(Number(data[6])),
+    helpfulness: Number(data[7])
+  });
+  lineNumber++;
+}
+
+console.log('Data Length', allData.length);
+console.log('End of Files');
+
+async function saveData () {
+  let chunkSize = 10;
+  for (let i = 0; i < allData.length; i ++) {
+    //const chunk = allData.slice(i, i + chunkSize);
+    console.log('I: ', allData[i].date);
+    await Question.create(allData[i]);
+  }
+}
+saveData();
 // Data Order
 
 // ID
@@ -30,33 +78,20 @@ const csv = require('csv-parser');
 // })
 
 
-const data = [];
-fs.createReadStream('./server/database/etl/questions.csv')
-  .pipe(csv())
-  .on('data', (row) => {
-    console.log('Push')
-    data.push(row);
-  })
-  .on('end',  async () => {
-    console.log('At End')
-    // Use the bulkInsert method of the model to insert the data
-    const chunkSize = 1000; // Set the chunk size to a value that works for your system
-    for (let i = 0; i < data.length; i += chunkSize) {
-      const chunk = data.slice(i, i + chunkSize);
-      await Question.bulkInsert(chunk);
-    }
-  });
+// const data = [];
+// fs.createReadStream('./server/database/etl/questions.csv')
+//   .pipe(csv())
+//   .on('data', (row) => {
+//     console.log('Push')
+//     data.push(row);
+//   })
+//   .on('end',  async () => {
+//     console.log('At End')
+//     // Use the bulkInsert method of the model to insert the data
+//     const chunkSize = 1000; // Set the chunk size to a value that works for your system
+//     for (let i = 0; i < data.length; i += chunkSize) {
+//       const chunk = data.slice(i, i + chunkSize);
+//       await Question.bulkInsert(chunk);
+//     }
+//   });
 
-
-const createData = (data) => {
-  Question.create({
-    id: Number(data[0]),
-    product_id: Number(data[1]),
-    body: data[2],
-    date: data[3],
-    asker_name: data[4],
-    asker_email: data[5],
-    reported: Boolean(Number(data[6])),
-    helpfulness: Number(data[7])
-  })
-}
